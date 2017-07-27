57f2428fb0c468eda7ea93d4e856eb042c06fb6b
Michael Levy <mikelevy@Michaels-MacBook-Pro.local>
2017-07-26 22:38:32 -0400

    -Using gulp for compiling and building scripts -ignore file

---
+++ gulp/assets.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var stripDebug = require('gulp-strip-debug');
+var concat = require('gulp-concat-multi');
+var source = [
+  'source/js/**/*.js'
+];
+var globalJs = 'source/js/globals.js';
+var rename = require('gulp-rename');
+var concatSrc = {
+  'accordion.js': [globalJs, 'source/js/raw/accordion.js'],
+  'carousel.js': [globalJs, 'source/js/raw/carousel.js'],
+  'input.js': [globalJs, 'source/js/raw/input.js'],
+  'modal.js': [globalJs, 'source/js/raw/modal.js'],
+  'radiocheck.js': [globalJs, 'source/js/raw/radiocheck.js'],
+  'select.js': [globalJs, 'source/js/raw/select.js'],
+  'tooltip.js': [globalJs, 'source/js/raw/tooltip.js'],
+  'toggler.js': [globalJs, 'source/js/raw/toggler.js']
+};
+
+// JS Linting
+gulp.task('lint', function() {
+  return gulp.src(source)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('source/css/*')
+    .pipe(gulp.dest('dist/css'));
+})
+gulp.task('uglify', function () {
+  concat(concatSrc)
+  .pipe(uglify())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('dist/js'));
+})
+
+gulp.task('scripts', ['lint', 'uglify'], function() {
+  concat(concatSrc)
+  .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('custom-build', ['clean-js'], function() {
+  var arg = process.argv.slice(3)[1];
+  var scripts = arg.split(',');
+  var buildScripts = [globalJs];
+
+  if (scripts[0] === 'all') {
+    buildScripts.push('source/js/raw/*.js');
+  } else {
+    for (var i = 0, len = scripts.length; i < len; i++) {
+      buildScripts.push('source/js/raw/'+scripts[i]+'.js');
+    }
+  }
+
+  concat({'scripts.js': buildScripts})
+  .pipe(gulp.dest('dist/js'))
+  .pipe(uglify())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('dist/js'));
+});
+
+
