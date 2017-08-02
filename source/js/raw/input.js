1782c77c0ec48ce4f383b410ac7f9d3e9391a4c6
Michael Levy <mikelevy@Michaels-MacBook-Pro.local>
2017-08-02 09:22:16 -0400

    [input control] -hbs integrated -commented -reformatted code/cleaned up

--- source/js/raw/input.js
+++ source/js/raw/input.js
@@ -1,127 +1,110 @@
+/**
+ * Input placeholder attribute polyfill. By default all inputs on the page will be passed
+ * into this class. 
+ * @constructor
+ * @param {HTMLElement} input The input element.
+ * @example
+ * var input = new ML.InputControl(ML.$('newInput'));
+ */
+ML.InputControl = function(input) {
+  var placeholder = ML.El.getAttr(input, 'placeholder');
+  var cursorTimer = null;
+
+  /**
+   * Events bound to the input.
+   * @private
+   */
+  function bindEvents() {
+    ML.El.evt(input, 'focus', function (e){
+      var inp = ML.El.clicked(e);
+      ML.addClass(inp, 'focus');
+      moveCursor(inp);
+      cursorTimer = setTimeout(function() {moveCursor(inp);}, 1);
+    });
+
+    ML.El.evt(input, 'blur', function (e){
+      ML.removeClass(ML.El.clicked(e), 'focus');
+      clearTimeout(cursorTimer);
+    });
+
+    ML.El.evt(input, 'click', function (e) {
+      if (ML.hasClass(ML.El.clicked(e), 'focus')) moveCursor(ML.El.clicked(e));
+    });
+
+    if (placeholder !== null) {
+      ML.El.evt(input, 'keyup', function (e) {
+        clearUnclear(ML.El.clicked(e));
+      });
+    }
+  }
+
+  /**
+   * Moves the cursor to the beginning of the text in the input. 
+   * This replicates the same effect of the "placeholder" attribute in supported browsers. 
+   * Credits: http://stackoverflow.com/questions/8189384/i-want-to-put-cursor-in-beginning-of-text-box-onfocus
+   * @param {HTMLElement} input The input field.
+   * @private
+   */
+  function moveCursor(input) {
+    if (!ML.hasClass(input, 'placeholder')) {
+      return;
+    } else {
+      if (typeof input.selectionStart === 'number') {
+        input.selectionStart = input.selectionEnd = 0;
+      } else if (typeof input.createTextRange !== 'undefined') {
+        input.focus();
+        var range = input.createTextRange();
+        range.collapse(true);
+        range.select();
+      }
+    }
+  }
+
+  /**
+   * Removes the current value in the input field. 
+   * Replicates the "placeholder" functionality in supported browsers.
+   * @param {HTMLElement} input The input field.
+   * @private
+   */
+  function clearUnclear(input) {
+    var old = ML.El.getAttr(input, 'placeholder');
+    var neww = input.value.replace(old, '');
+
+    if (ML.hasClass(input, 'placeholder')) input.value = neww;
+    ML.removeClass(input, 'placeholder');
+
+    // No characters in input field
+    if (input.value.length < 1) {
+      input.value = old;
+      ML.addClass(input, 'placeholder');
+      moveCursor(input);
+    }
+  } 
+
+  if (placeholder !== null) {
+    input.setAttribute('value', placeholder);
+    ML.addClass(input, 'placeholder');
+    bindEvents();
+  }                 
+};
+
 (function () {
-	/**
-	* @function PHsupported
-	* Find out if the "placeholder" attribute is supported.
-	*/
-	var PHsupported = function () {
-		var createInput = ML.El.create('input');
-		return ('placeholder' in createInput) ? true : false;
-	}
+  /**
+   * Returns true/false if placeholder attribute is supported.
+   * @return {boolean}
+   */
+  var placeholderSupport = function () {
+    var createInput = ML.El.create('input');
+    return ('placeholder' in createInput) ? true : false;
+  };
 
-	/**
-	* @class InputControl
-	* @namespace ML
-	* Adds placeholder functionality if not supported and adds a class of "focus" when input is focused.
-	*
-	* @param {String} ph - value of "placeholder" attribute.
-	* @param {HTMLElement} el - input field.
-	* @param {function} cursorTimer - timer for activating the move cursor function, needed for Chrome.
-	*/
-	ML.InputControl = function (input) {
-		return {
-			ph: ML.El.getAttr(input, 'placeholder'),
-			el: input,
-			cursorTimer: null,
+  if (!placeholderSupport()) {
+    var inputs = ML._$('input');
 
-			/**
-			* @function init
-			* Initialization of input control.
-			* If a placeholder attribute is on the input, takes the value and places it into the value attribute.
-			*/
-			init: function () {
-				if (this.ph !== null) {
-					this.el.setAttribute('value', this.ph);
-					ML.addClass(this.el, 'placeholder');
-				}
-			
-				this.bindEvents();
-			},
-
-			/**
-			* @function bindEvents
-			* Binds events to the input(s).
-			*/
-			bindEvents: function () {
-				var self = this;
-				
-				ML.El.evt(self.el, 'focus', function (e){
-					var inp = ML.El.clicked(e);
-				
-					ML.addClass(inp, 'focus');
-				
-					self.moveCursor(inp);
-					self.cursorTimer = setTimeout(function() {self.moveCursor(inp)}, 1);
-				});
-				
-				ML.El.evt(self.el, 'blur', function (e){
-					ML.removeClass(ML.El.clicked(e), 'focus');
-					clearTimeout(self.cursorTimer);
-				});
-				
-				ML.El.evt(self.el, 'click', function (e) {
-					if (ML.hasClass(ML.El.clicked(e), 'focus')) self.moveCursor(ML.El.clicked(e));
-				});
-				
-				if (self.ph !== null) {
-					ML.El.evt(input, 'keyup', function (e) {self.clearUnclear(ML.El.clicked(e));});
-				}
-			},
-
-			/**
-			* @function moveCursor
-			* When input is focused. This will move the cursor to the beginning of the text in the input.
-			* This is to replicate the same effect of the "placeholder" attribute in supported browsers.
-			* Credits: http://stackoverflow.com/questions/8189384/i-want-to-put-cursor-in-beginning-of-text-box-onfocus
-			*
-			* @param {HTMLElement} inp - input focused/clicked on.
-			*/
-			moveCursor: function (inp) {
-				if (!ML.hasClass(inp, 'placeholder')) {
-					return;
-				} else {
-					if (typeof inp.selectionStart == "number") {
-						inp.selectionStart = inp.selectionEnd = 0;
-					} else if (typeof inp.createTextRange != "undefined") {
-						inp.focus();
-						var range = inp.createTextRange();
-						range.collapse(true);
-						range.select();
-					}
-				}
-			},
-
-			/**
-			* @function clearUnclear
-			* Once user starts typing in the input field. This removes the current value in the input field.
-			* Replicates the "placeholder" functionality in supported browsers.
-			*
-			* @param {HTMLElement} _input - input field.
-			*/
-			clearUnclear: function (_input) {
-				var old = ML.El.getAttr(_input, 'placeholder'),
-					_new = _input.value.replace(old, '');
-			
-				if (ML.hasClass(_input, 'placeholder')) _input.value = _new;
-				ML.removeClass(_input, 'placeholder');
-			
-				// No characters in input field
-				if (_input.value.length < 1) {
-					_input.value = old;
-					ML.addClass(_input, 'placeholder');
-					this.moveCursor(_input);
-				}                                       
-			}
-		}
-	}
-
-	// If "placeholder" attribute is not supported Loop around all <input> tags.
-	if (!PHsupported()) {
-		var allInputs = ML._$('input');
-		for (var i=0; i<allInputs.length; i++) {
-			if(allInputs[i].type == 'text') {
-				var ic = new ML.InputControl(allInputs[i]);
-				ic.init();
-			}
-		}
-	}
+    for (var i = 0, len = inputs.length; i < len; i++) {
+      if (inputs[i].type == 'text') {
+        new ML.InputControl(inputs[i]);
+      }
+    }
+  }
 }());
