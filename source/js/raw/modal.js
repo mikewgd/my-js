2a557f634068f254926403c31fc79e760c5a7a84
Michael Levy <michael.levy@rga.com>
2017-08-11 16:46:56 -0400

    [modal] notes

--- source/js/raw/modal.js
+++ source/js/raw/modal.js
@@ -3,47 +3,50 @@
 
 'use strict';
 
-ML.Modal = function(el, settings) {
+// use this https://scotch.io/tutorials/building-your-own-javascript-modal-plugin
+// for inspiration. define a template and have content injected inside.
+// to transfer events need to keep track of them using ML.evtHandler.
+
+ML.Modal = function(settings) {
 	var DEFAULTS = {
-    WIDTH: 600,
-    HEIGHT: 'auto',
-    TITLE: ''
+    maxWidth: 600,
+    minWidth: 280,
+    closeButton: true,
+    autoOpen: false
   };
 
   var CLASSNAMES = {
-  	TITLE: 'modal-header-title',
-  	CLOSE: 'modal-close',
-  	EL: 'modal',
-  	BODY: 'modal-body'
+  	el: 'modal',
+  	body: 'modal-body',
+    close: 'modal-close'
   };
 
 	var modal = null;
-	var modalBody = null;
 	var width = settings.width || DEFAULTS.WIDTH;
 	var height = settings.height || DEFAULTS.HEIGHT;
 	var title = settings.title || DEFAULTS.TITLE;
 
 	this.init = function() {
     modal = ML.$(el);
-		ML.El.clean(modalOverlay);
 		modalBody = modal.childNodes[0];
 
-		bindEvents();
+    create();
+		// bindEvents();
 	};
+
+  function create() {
+
+  }
 
 	function bindEvents() {
     /* jshint validthis: true */
 		var self = this;
 
-    /*ML.El.evt(window, 'resize', function(e) {
-    	resize();
-    });*/
-
     ML.El.evt(document, 'click', function(e) {
     	e.preventDefault();
       var clicked = ML.El.clicked(e);
-      if (ML.hasClass(clicked, CLASSNAMES.CLOSE) ||
-      	ML.hasClass(clicked, CLASSNAMES.OVERLAY)) {
+      if (ML.hasClass(clicked, ) ||
+      	ML.hasClass(clicked, CLASSNAMES.EL)) {
         self.hide();
       }
     });
@@ -59,23 +62,15 @@ ML.Modal = function(el, settings) {
     	'width': width + 'px',
     	'height': (height === 'auto') ? 'auto' : height + 'px',
     	'marginTop': '-' + (height / 2) + 'px',
-    	'marginLeft': '-' + (+.offsetWidth / 2) + 'px'
+    	'marginLeft': '-' + (modalBody.offsetWidth / 2) + 'px'
     });
 	};
 
-	this.hide = function() {};
+	this.hide = function() {
+
+  };
 };
 
 (function() {
-  var modalLink = ML._$('*');
-  var modal = null;
-  var settings = {};
 
-  for (var i = 0; i < modalLink.length; i++) {
-    if (ML.El.data(modalLink[i], 'modal') !== null) {
-      settings = ML.parObj(ML.El.data(modalLink[i], 'modal'));
-      modal = new ML.Modal(modalLink[i], settings);
-      modal.init();
-    }
-  }
 })();
