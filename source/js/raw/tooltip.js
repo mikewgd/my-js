ee479e57ac17d5a0afe8d87a0ae8d548492196ff
Michael Levy <mikelevy@Michaels-MacBook-Pro.local>
2017-08-19 19:25:50 -0400

    [tooltip] -comments -class name adjustment

--- source/js/raw/tooltip.js
+++ source/js/raw/tooltip.js
@@ -4,6 +4,20 @@
 (function () {
   'use strict';
 
+   /**
+   * Tooltip
+   * @constructor
+   * @param {object} [settings] Configuration settings.
+   * @param {string} [settings.selectorToggle=data-tooltip] The selector when mouseover shows the tooltip.
+   * @param {string} [settings.selectorModal=tooltip] The selector for the tooltip.
+   * @param {string} [settings.activeclass=active] The class to show the tooltip.
+   * @param {number} [settings.width=600] The width of the tooltip.
+   * @param {boolean} [settings.arrow=true] Shows an arrow.
+   * @param {string} [settings.align=right] Where the tooltip should be positioned according to the link.
+   * @example
+   * var tooltips = new ML.Tooltip();
+   * tooltips.init();
+   */
   ML.Tooltip = function(settings) {
     /**
      * Tooltip defaults.
@@ -14,21 +28,38 @@
       selectorTip: 'data-tooltip',     // attribute
       selectorTooltip: 'tooltip',      // class name, can change to attr
       activeClass: 'active',
-      width: 100
+      width: 100,
+      arrow: true,
+      align: 'right'
     };
+
+    /**
+     * Tooltip alignment class names.
+     * @type {array}
+     * @private
+     */
+    var ALIGNMENT_CLASSES = [
+      'tooltip-left-align',
+      'tooltip-right-align',
+      'tooltip-top-align',
+      'tooltip-bottom-align'
+    ];
 
     var options = {};
     var tooltips = [];
     var tips = [];
     var self = this;
 
+    /**
+     * Initializes the tooltip class.
+     */
     this.init = function() {
       var tags = ML.El._$('*');
 
-      // self.destroy();
+      self.destroy();
 
       options = ML.extend(DEFAULTS, settings);
-      modals = ML.El.$C(options.selectorTooltip);
+      tooltips = ML.El.$C(options.selectorTooltip);
 
       ML.loop(tags, function(element) {
         if (element.getAttribute(options.selectorTip) !== null) {
@@ -36,197 +67,163 @@
         }
       });
 
-      // bindEvents();
+      bindEvents();
     };
 
-    function bindEvents() {}
+    /**
+     * Events bound to elements.
+     * @private
+     */
+    function bindEvents() {
+      ML.loop(tips, function(element) {
+        ML.El.evt(element, 'mouseover', mouseOver);
+        ML.El.evt(element, 'mouseout', mouseOut);
+      });
+    }
 
+    /**
+     * Handler bound to showing the tooltip.
+     * @param {Event} e The Event object.
+     * @private
+     */
+    function mouseOver(e) {
+      var clicked = ML.El.clicked(e);
+      self.show(clicked, ML.parObj(ML.El.data(clicked, 'tooltip')));
+    }
+
+    /**
+     * Handler bound to hiding the tooltip.
+     * @param {Event} e The Event object.
+     * @private
+     */
+    function mouseOut(e) {
+      self.hide();
+    }
+
+    /**
+     * Destroys an instance of the tooltip class.
+     */
     this.destroy = function() {
       // remove event listeners.
-      // @TODO
+      ML.loop(tips, function(element) {
+        element.removeEventListener('mouseover', mouseOver, false);
+        element.removeEventListener('mouseout', mouseOut, false);
+      });
+
+      ML.loop(tooltips, function(element) {
+        element.removeAttribute('style');
+        ML.El.removeClass(element, ALIGNMENT_CLASSES.join(' '), true);
+        ML.El.removeClass(element, element._options.activeClass);
+      });
 
       options = null;
-      overlay = null;
-      modals = [];
-      toggles = [];
+      tooltips = [];
+      tips = [];
     };
 
-    this.show = function(id, tooltipOptions) {};
+    /**
+     * Shows a tooltip.
+     * @param {HTMLElement} tip The link when moused over to show tooltip.
+     * @param {object} tooltipOptions Configuration settings to overwrite defaults.
+     */
+    this.show = function(tip, tooltipOptions) {
+      var tooltip = updateTooltips(ML.El.$(tip.rel), ML.extend(options, tooltipOptions));
+      var arrow = null;
 
-    this.hide = function() {};
+      if (tooltip._options.arrow) {
+        if (ML.El.$C('tooltip-arrow', tooltip).length > 0) {
+          arrow = ML.El.$C('tooltip-arrow', tooltip);
+        } else {
+          arrow = ML.El.create('span', {'class': 'tooltip-arrow'});
+          tooltip.appendChild(arrow);
+        }
+      }
+
+      ML.El.addClass(tooltip, tooltip._options.activeClass);
+      ML.El.removeClass(tooltip, ALIGNMENT_CLASSES.join(' '), true);
+      ML.El.addClass(tooltip, 'tooltip-' + tooltip._options.align + '-align');
+
+      tooltip.style.width = tooltip._options.width + 'px';
+      setPosition(tip, tooltip, tooltip._options.align);
+    };
+
+    /**
+     * Updates the tooltips array.
+     * @param {HTMLElement} el The tooltip to search for in array.
+     * @param {object} options The options for the tooltip.
+     * @return {HTMLElement} The tooltip found in the array of modals.
+     * @private
+     */
+    function updateTooltips(el, options) {
+      var tooltip = {};
+
+      for (var i = 0, len = tooltips.length; i < len; i++) {
+        if (el.id === tooltips[i].id) {
+          tooltip = tooltips[i];
+          tooltips[i]._options = options;
+        }
+      }
+
+      return tooltip;
+    }
+
+    /**
+     * Sets the position of the tooltip
+     * @param {HTMLElement} tip The link when moused over to show the tooltip.
+     * @param {HTMLElement} tooltip The tooltip element.
+     * @param {string} align Where to position the tooltip element.
+     * @private
+     */
+    function setPosition(tip, tooltip, align) {
+      var tipDimens = ML.El.dimens(tip);
+      var tooltipDimens = ML.El.dimens(tooltip);
+
+      switch (align) {
+        case 'right':
+          ML.El.setStyles(tooltip, {
+            top: Math.abs((tooltipDimens.height / 2) - tipDimens.y - tipDimens.height) + 'px',
+            left: tipDimens.x  + tipDimens.width + 'px'
+          });
+          break;
+
+        case 'left':
+          ML.El.setStyles(tooltip, {
+            top: Math.abs((tooltipDimens.height / 2) - tipDimens.y - tipDimens.height) + 'px',
+            left: tipDimens.x - tooltip.offsetWidth + 'px'
+          });
+          break;
+
+        case 'top':
+          ML.El.setStyles(tooltip, {
+            top: tipDimens.y - tooltipDimens.height + 'px',
+            left: tipDimens.x - (tooltip.offsetWidth / 2) + (tipDimens.width / 2) + 'px'
+          });
+          break;
+
+        case 'bottom':
+          ML.El.setStyles(tooltip, {
+            top: tipDimens.y + tipDimens.height + 'px',
+            left: tipDimens.x - (tooltip.offsetWidth / 2) + (tipDimens.width / 2) + 'px'
+          });
+          break;
+      }
+    }
+
+    /**
+     * Hides all the tooltips.
+     */
+    this.hide = function() {
+      ML.loop(tooltips, function(tooltip) {
+        ML.El.removeClass(tooltip, tooltip._options.activeClass);
+      });
+    };
+
+    /**
+     * Returns all registered tooltips.
+     * @return {array} All the registered modals with custom options, "_options".
+     */
+    this.getAll = function() {
+      return tooltips;
+    };
   };
 })();
-
-// /**
-// * @class TooltipHandler
-// * @namespace ML
-// * Handles calling the Tooltip class.
-// */
-// ML.TooltipHandler = function () {
-// 	var tags = ML.El._$('*', document.body);
-
-// 	for (var i=0; i<tags.length; i++) {
-// 		var attr = ML.El.data;
-
-// 		if (attr(tags[i], 'tooltip') !== null) {
-// 			if (tags[i].title) tags[i].rel = 'mlTooltip-'+i;
-// 			var t = new ML.Tooltip(tags[i],  ML.ParObj({elem: ML.El.data(tags[i], 'tooltip')}));
-// 			t.init();
-// 		}
-// 	}
-// };
-
-// /**
-// * @class Tooltip
-// * @namespace ML
-// *
-// * @property {Number} width - width of tooltip.
-// * @property {String} direction - where to show the the tooltip.
-// * @property {HTMLElement} link - element that activates tooltip, does not need to be a link.
-// * @property {HTMLElement} el - the tooltip.
-// * @property {String} template - standard template/HTML structure of a tooltip.
-// * @property {HTMLElement} arrow - arrow element in tooltip.
-// */
-// ML.Tooltip = function(tip, settings) {
-// 	var defaults = {width: 100, direction: 'right'};
-
-// 	return {
-// 		width: parseInt(settings.width) || defaults.width,
-// 		direction: settings.direction || defaults.direction,
-// 		link: tip,
-// 		el: null,
-// 		template: '<span class="arrow"></span><p class="content">{{message}}</p>',
-// 		arrow: null,
-
-// 		/**
-// 		* @function init
-// 		* Initialization of functions. Also creates the dynamic tooltip.
-// 		*/
-// 		init: function () {
-// 			// if no rel attribute return, tooltip will not work.
-// 			if (this.link.rel == null || this.link.rel == undefined) {return false;}
-
-// 			if (this.link.title){
-// 				this.create(this.link);
-// 				this.link.removeAttribute('title');
-// 			}
-
-// 			this.el = ML.$(this.link.rel);
-
-// 			this.bindEvents();
-// 		},
-
-// 		/**
-// 		* @function bindEvents
-// 		* Binds events to the link (tooltip activation).
-// 		*/
-// 		bindEvents: function() {
-// 			var self = this;
-
-// 			ML.El.evt(self.link, 'mouseover', function(e) {self.show();});
-// 			ML.El.evt(self.link, 'mouseout', function(e) {self.hide();});
-// 		},
-
-// 		/**
-// 		* @function create
-// 		* Creates a custom tooltip(s) and adds them to the DOM.
-// 		*
-// 		* @param {HTMLElement} dyn - element that needs a custom tooltip.
-// 		*/
-// 		create: function(dyn) {
-// 			var div = ML.El.create('div', {'class': 'tooltip hidden', id: dyn.rel});
-// 			div.innerHTML = this.template.replace('{{message}}', dyn.title);
-// 			document.body.appendChild(div);
-// 		},
-
-// 		*
-// 		* @function show
-// 		* Shows the tooltip and put the set width on it and etc...
-// 		*
-// 		* @param {Boolean} selfInvoke (optional) - if invoking the tooltip manually.
-
-// 		show: function (selfInvoke) {
-// 			var self = this;
-
-// 			if (selfInvoke) {self.el = ML.$(tip.rel);}
-
-// 			var tooltip = self.el,
-// 				tags = tooltip.getElementsByTagName('*');
-
-// 			self.hide(); // added to close all active tooltips.
-// 			ML.removeClass(tooltip, 'hidden');
-// 			ML.removeClass(tooltip, 'top bottom left right', true);
-// 			ML.addClass(tooltip, self.direction);
-// 			tooltip.style.width = self.width+'px';
-
-// 			for (var i=0; i<tags.length; i++) {
-// 				if (ML.hasClass(tags[i], 'arrow')) self.arrow = tags[i];
-// 			}
-
-// 			self.positionTooltip();
-// 		},
-
-// 		/**
-// 		* @function hide
-// 		* Hides all tooltips.
-// 		*/
-// 		hide: function() {
-// 			var tooltips = ML.$C('.tooltip');
-
-// 			for (var i=0; i<tooltips.length; i++) {
-// 				ML.removeClass(tooltips[i], 'top bottom left right', true);
-// 				ML.addClass(tooltips[i], 'hidden');
-// 			}
-// 		},
-
-// 		/**
-// 		* @function positionTooltip
-// 		* Positions the tooltip based on the direction set.
-// 		*/
-// 		positionTooltip: function() {
-// 			var tooltip = this.el, arrow = this.arrow,
-// 				linkDimens = ML.El.dimens(this.link),
-// 				tipDimens = ML.El.dimens(tooltip);
-
-// 			this.arrow.removeAttribute('style');
-
-// 			switch (this.direction) {
-// 				case 'right':
-// 					ML.El.styl(tooltip, {
-// 						top: Math.abs((tipDimens.height/2) - linkDimens.y - linkDimens.height)-arrow.offsetWidth + 'px',
-// 						left: linkDimens.x  + linkDimens.width + 'px'
-// 					});
-
-// 					ML.El.styl(arrow, {left:0, top:(tipDimens.height/2)-(arrow.offsetHeight/2) + 'px'});
-// 				break;
-
-// 				case 'left':
-// 					ML.El.styl(tooltip, {
-// 						top: Math.abs((tipDimens.height/2) - linkDimens.y - linkDimens.height)-arrow.offsetWidth + 'px',
-// 						left: linkDimens.x-tooltip.offsetWidth + 'px'
-// 					});
-
-// 					ML.El.styl(arrow, {right:0, top:(tipDimens.height/2)-(arrow.offsetHeight/2) + 'px'});
-// 				break;
-
-// 				case 'top':
-// 					ML.El.styl(tooltip, {
-// 						top: linkDimens.y - tipDimens.height + 'px',
-// 						left: linkDimens.x - (tooltip.offsetWidth/2) + (linkDimens.width/2) + 'px'
-// 					});
-
-// 					ML.El.styl(arrow, {bottom:0, left:(tooltip.offsetWidth/2)  + 'px'});
-// 				break;
-
-// 				case 'bottom':
-// 					ML.El.styl(tooltip, {
-// 						top: linkDimens.y + linkDimens.height + 'px',
-// 						left: linkDimens.x - (tooltip.offsetWidth/2) + (linkDimens.width/2) + 'px'
-// 					});
-
-// 					ML.El.styl(arrow, {top:0, left:(tooltip.offsetWidth/2)  + 'px'});
-// 				break;
-// 			}
-// 		}
-// 	}
-// };
