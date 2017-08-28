bd7fab0a9b45ff4a992ab15e6d562361783a345a
Michael Levy <mikelevy@Michaels-MacBook-Pro.local>
2017-08-28 17:20:57 -0400

    [accordion] -destroy instance -destroy button (to test) -initializing accordion manually -started using hash

--- source/js/raw/accordion.js
+++ source/js/raw/accordion.js
@@ -14,19 +14,41 @@
    */
   ML.Accordion = function(el, multiple) {
     var lis = [];
+    var self = this;
+    var storedHash = window.location.hash;
+    var interval = null;
+
     /**
      * Initialization of accordion component.
-     * @private
      */
-    function init() {
+    this.init = function() {
       try {
+        self.destroy();
+
         lis = ML.El._$('li', el);
         hideLis();
         bindEvents();
       } catch(e) {
         throw new Error(e);
       }
-    }
+    };
+
+    this.destroy = function() {
+      if (lis.length > 0) {
+        ML.loop(lis, function(item, index) {
+          ML.El.removeClass(item, 'accordion-hide-content');
+          ML.El.removeClass(item, 'accordion-open-content');
+        });
+
+        ML.loop(ML.El._$('a', el), function(item) {
+          if (ML.El.hasClass(item, 'accordion-toggle')) {
+            item.removeEventListener('click', toggle, false);
+          }
+        });
+
+        lis = [];
+      }
+    };
 
     /**
      * Adds "hide" class to <li> elements.
@@ -60,41 +82,61 @@
      * @private
      */
     function bindEvents() {
+      /*if ('onhashchange' in window) { // event supported?
+        window.onhashchange = function () {
+          console.log(window.location.hash);
+        }
+      } else { // event not supported:
+        interval = setInterval(function () {
+          console.log('set interval')
+          if (window.location.hash !== storedHash) {
+            storedHash = window.location.hash;
+          }
+        }, 100);
+      }*/
+
       ML.loop(ML.El._$('a', el), function(item) {
         if (ML.El.hasClass(item, 'accordion-toggle')) {
-          ML.El.evt(item, 'click', function(e) {
-            e.preventDefault();
-            toggle(ML.El.clicked(e).parentNode);
-          });
+          ML.El.evt(item, 'click', toggle);
         }
       });
     }
 
+    function openTab(hash) {
+      
+    }
+
     /**
      * Handles toggling the <li> element.
-     * @param {HTMLElement} li Parent element to accordion toggle, i.e. <li>
+     * @param {Event} e The clicked element.
      * @private
      */
-    function toggle(li) {
+    function toggle(e) {
+      var li = ML.El.clicked(e).parentNode;
+      e.preventDefault();
+
+      console.log(li, 'toggle')
+
       if (multiple) {
         ML.El.toggleClass(li, 'accordion-hide-content');
       } else {
         hideLis(li);
       }
     }
-
-    init();
   };
 })();
 
 (function() {
   var accordion = ML.El._$('*');
   var toggle = false;
+  var acc = null;
 
   for (var i = 0; i < accordion.length; i++) {
     if (ML.El.data(accordion[i], 'accordion') !== null) {
       toggle = (ML.El.data(accordion[i], 'accordion') === 'multiple') ? true : false;
-      new ML.Accordion(accordion[i], toggle);
+      acc = new ML.Accordion(accordion[i], toggle)
+
+      acc.init();
     }
   }
 })();
