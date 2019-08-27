833b79922d3529403121bd7d4fcb2a1f2d0c0574
michael levy <mike.levy75@gmail.com>
2019-08-27 09:06:44 -0400

    [browser support] remove preventDefault and computed style poly

--- source/js/globals.js
+++ source/js/globals.js
@@ -2,36 +2,6 @@
 
 'use strict';
 
-// Polyfill: window.getComputedStyle
-if (!window.getComputedStyle) {
-  window.getComputedStyle = function(el) {
-    this.el = el;
-    this.getPropertyValue = function(prop) {
-      var re = /(\-([a-z]){1})/g;
-      if (prop === 'float') {
-        prop = 'styleFloat';
-      }
-
-      if (re.test(prop)) {
-        prop = prop.replace(re, function() {
-          return arguments[2].toUpperCase();
-        });
-      }
-
-      return el.currentStyle[prop] ? el.currentStyle[prop] : null;
-    };
-
-    return this;
-  };
-}
-
-// Polyfill: Event.preventDefault()
-if (!Event.prototype.preventDefault) {
-  Event.prototype.preventDefault = function() {
-    this.returnValue = false;
-  };
-}
-
 var ML = {} || function() {};
 window.ML = window.ML || function() {};
 
