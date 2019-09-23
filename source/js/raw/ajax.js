f34ab2f7769bb75b9513111638dc228f9cfa1aa6
michael levy <mike.levy75@gmail.com>
2019-09-22 20:42:20 -0400

    [ie67] - sep files for ajax and animate - no breakpoints file

---
+++ source/js/raw/ajax.js
@@ -0,0 +1,190 @@
+/**
+ * * Vanilla ajax requests
+ * * Supports `GET`, `POST`, `PUT`, `DELETE`, `JSONP` methods.
+ * * When using `JSONP` request, you need to setup a global callback function and
+ * pass in the function name as a string in `jsonpCallback`.
+ *
+ * @example <caption>GET Request</caption>
+ * new ML.Ajax({
+ *   responseType: 'json',
+ *   url: 'https://reqres.in/api/users/2',
+ *   success: function(xhr) {
+ *     console.log(xhr.response.data);
+ *   },
+ *   error: function(xhr) {
+ *     console.log('ERROR', xhr);
+ *   }
+ * });
+ *
+ * @example <caption>POST Request</caption>
+ * new ML.Ajax({
+ *   url: 'https://reqres.in/api/users',
+ *   method: 'POST',
+ *   responseType: 'json',
+ *   data: {
+ *     name: 'john smith',
+ *     age: 22
+ *   },
+ *   success: function(xhr) {
+ *     console.log(xhr.response);
+ *   },
+ *   error: function(xhr) {
+ *     console.log('ERROR', xhr);
+ *   }
+ * });
+ *
+ * @example <caption>JSONP Request</caption>
+ * var callbackFunction = function(response) {
+ *   console.log(response);
+ * };
+ * 
+ * new ML.Ajax({
+ *   url: 'https://jsfiddle.net/echo/jsonp',
+ *   method: 'JSONP',
+ *   data: {
+ *     name: 'john smith',
+ *     age: 22
+ *   },
+ *   jsonpCallback: 'callbackFunction'
+ * });
+ * 
+ * @param {Object} params Configuration settings.
+ * @param {String} [params.method=GET] The type of request.
+ * @param {String} params.url The URL to make a request to.
+ * @param {Object} [params.headers={'Content-type': 'application/x-www-form-urlencoded'}] Adds headers to your request: `request.setRequestHeader(key, value)`
+ * @param {String} [params.responseType=text] Format of the response. [info](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/responseType)
+ * @param {Boolean} [params.cors=false] Cross domain request. 
+ * @param {Object|String} [params.data=null] Data to send with the request.
+ * @param {String} params.jsonpCallback The name of the function for JSONP callback.
+ * @param {Function} params.success If the request is successful. XHR is returned.
+ * @param {Function} params.error If the request errors out. XHR is returned.
+ * @constructor
+ */
+ML.Ajax = function(params) {
+  /**
+   * Ajax defaults.
+   * @type {Object}
+   * @private
+   */
+  var DEFAULTS = {
+    method: 'GET',
+    url: null,
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded'
+    },
+    responseType: 'text',
+    cors: false, 
+    data: null,
+    jsonpCallback: '',
+    success: function() {},
+    error: function() {}
+  };
+
+  var xhr = null;
+  var options = ML.extend(DEFAULTS, (ML.isUndef(params, true)) ? {} : params);
+
+  /**
+   * Initialization of ajax.
+   * @private
+   */
+  function init() {
+    if (window.location.host === '') {
+      throw new Error('Must be hosted on a server.');
+    } else {
+      xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+      options.method = options.method.toString().toUpperCase();
+      options.cors = ML.bool(options.cors);
+
+      if (!/[^/]+$/.test(options.url)) {
+        throw new Error('Not a valid URL.');
+      }
+
+      if (!/^GET$|^POST$|^JSONP$|^PUT$|^DELETE$/.test(options.method)) {
+        options.method = DEFAULTS.method;
+      }
+
+      if (ML.isUndef(options.responseType, true)) {
+        options.responseType = DEFAULTS.responseType;
+      }
+
+      if (!ML.isBool(options.cors)) {
+        options.cors = DEFAULTS.cors;
+      }
+
+      if ((typeof options.data) !== 'object') {
+        options.data = DEFAULTS.data;
+      } else {
+        options.data = ML.urlParams(options.data);
+      }
+
+      if (options.method === 'JSONP') {
+        jsonpRequest();
+      } else {
+        xhrRequest();
+      }
+    }
+  }
+
+  /**
+   * JSONP Request.
+   * @private
+   */
+  function jsonpRequest() {
+    var script = ML.El.create('script');
+    options.url += (options.url.indexOf('?') + 1 ? '&' : '?') + ML.urlParams(options.data);
+    options.url += (options.url.indexOf('?') + 1 ? '&' : '?') + 'callback=' + options.jsonpCallback;
+    script.src = options.url;
+
+    document.body.appendChild(script);
+  }
+
+  /**
+   * XHR Request: GET, POST, PUT, JSONP, DELETE
+   * @private
+   */
+  function xhrRequest() {
+    var readyState = function() {
+      // Only run if the request is complete
+      if (xhr.readyState !== 4) {
+        return;
+      }
+
+      if (xhr.status >= 200 && xhr.status < 300) {
+        options.success(xhr);
+      } else {
+        options.error(xhr);
+      }
+    };
+
+    ML.El.evt(xhr, 'readystatechange', readyState);
+
+    if (options.cors) {
+      if ('withCredentials' in xhr) {
+        xhr.withCredentials = true;
+      } else if (typeof XDomainRequest !== 'undefined') {
+        xhr = new XDomainRequest();
+      } else {
+        throw new Error('CORS is not supported.');
+      } 
+    }
+
+    xhr.open(options.method, options.url);
+    xhr.responseType = options.responseType;
+
+    for (var header in options.headers) {
+      if (options.headers.hasOwnProperty(header)) {
+        xhr.setRequestHeader(header, options.headers[header]);
+      }
+    }
+
+    xhr.send(options.data);
+  }
+
+  /**
+   * Returns the XHR.
+   * @return {Object|String}
+   */
+  this.xhr = xhr;
+  
+  init();
+};
---
+++ source/js/raw/animate.js
@@ -0,0 +1,178 @@
+
+/**
+ * * Easily animate CSS values. [credit](https://javascript.info/js-animation)
+ * * Only `px` values can be animated at this time.
+ * * Animated properties can be relative. If the value leads with  `+` or `-`, then 
+ * the target value is computed by adding or subtracting the given number from the current value of the property.
+ * * For now `opacity` can not be animated with a relative value. 
+ * * The following easing options are available: `linear` (default), `elastic`, `quad`,
+ * `quint`, `circ`, `back` or `bounce`.
+ * 
+ * @example
+ * var props = {width: 100, height: 100};
+ * var settings = {delay: 15, duration: 500, easing: 'bounce'};
+ * 
+ * new ML.Animate(ML.$('el'), props, settings, function() {
+ *   alert('animation is complete');
+ * });
+ * 
+ * @param {HTMLElement} el Element to animate.
+ * @param {Object} props CSS properties to animate.
+ * @param {Object} [settings] Configuration settings.
+ * @param {Number} [settings.duration=400] The duration of the animation, defaults to 400ms.
+ * @param {Number} [settings.delay=13] The delay of the animation, defaults to 13.
+ * @param {Boolean} [settings.relative=true] Ability to override animating relative values.
+ * @param {String} [settings.easing=linear] Type of animation (`bounce`, `elastic`, etc..), defaults to `linear`
+ * @param {Function} [cb] Callback function.
+ * @constructor
+ */
+ML.Animate = function(el, props, settings, cb) {
+  /**
+   * Animate defaults.
+   * @type {Object}
+   * @private
+   */
+  var DEFAULTS = {
+    duration: 400,
+    delay: 13,
+    easing: 'linear',
+    relative: true
+  };
+
+  var Easing = {
+    linear: function(progress) {
+      return progress;
+    },
+
+    elastic: function(progress) {
+      return Math.pow(2, 10 * (progress - 1)) * Math.cos(20 * Math.PI * 1.5 / 3 * progress);
+    },
+
+    quad: function(progress) {
+      return Math.pow(progress, 2);
+    },
+
+    quint: function(progress) {
+      return Math.pow(progress, 5);
+    },
+
+    circ: function(progress) {
+      return 1 - Math.sin(Math.acos(progress));
+    },
+
+    back: function(progress) {
+      return Math.pow(progress, 2) * ((1 + 1.5) * progress - 1.5);
+    },
+
+    bounce: function(progress) {
+      for (var a = 0, b = 1; 1; a += b, b /= 2) {
+        if (progress >= (7 - 4 * a) / 11) {
+          return -Math.pow((11 - 6 * a - 11 * progress) / 4, 2) + Math.pow(b, 2);
+        }
+      }
+    }
+  };
+
+  var options = ML.extend(DEFAULTS, (ML.isUndef(settings, true)) ? {} : settings);
+  var timer = null;
+  var currProps = {};
+  var progress = false;
+  var time = new Date();
+
+  /**
+   * Initialization of animating elements.
+   * @private
+   */
+  function init() {
+    if (ML.isUndef(el.tagName)) {
+      throw new Error('You can only animate a valid element on the page.');
+    } else {
+      if (!ML.isNum(options.duration)) {
+        options.duration = DEFAULTS.duration;
+      }
+
+      if (!ML.isNum(options.delay)) {
+        options.delay = DEFAULTS.delay;
+      }
+
+      if (!ML.isBool(options.relative)) {
+        options.relative = DEFAULTS.relative;
+      }
+
+      if (ML.isUndef(Easing[options.easing])) {
+        options.easing = Easing[DEFAULTS.easing];
+      } else {
+        options.easing = Easing[options.easing];
+      }
+    }
+
+    move();
+  }
+
+  /**
+   * Gets the current CSS values of the properties being animated.
+   * @private
+   */
+  function getCurrs() {
+    var currProp = '';
+    currProps = {};
+
+    for (var prop in props) {
+      currProp = parseFloat(ML.El.getStyle(el, prop).replace('px', ''));
+      currProps[prop] = currProp;
+    }
+  }
+
+  /**
+   * Fade in/out an element.
+   * @private
+   */
+  function fadeEl() {
+    var curr = (currProps.opacity * 100);
+    var desr = (props.opacity * 100);
+    var whole = Math.round(curr + (desr - curr) * options.easing(progress));
+
+    el.style.opacity = whole / 100;
+  }
+
+  /**
+   * Animates the element with the new CSS values provided.
+   * @private
+   */
+  function move() {
+    var value = 0;
+    getCurrs();
+
+    timer = setInterval(function() {
+      value = 0;
+      progress = (new Date() - time) / options.duration;
+
+      if (progress > 1) {
+        progress = 1;
+      }
+
+      for (var prop in props) {
+        if (prop === 'opacity') {
+          fadeEl();
+        } else {
+          if (options.relative && (/^\+/g.test(props[prop]) || /^\-/g.test(props[prop]))) {
+            value = Math.round(currProps[prop] + parseFloat(props[prop]) * options.easing(progress));
+          } else {
+            value = Math.round(currProps[prop] + (props[prop] - currProps[prop]) * options.easing(progress));
+          }
+          
+          el.style[prop] = value + 'px';
+        }
+      }
+
+      if (progress === 1) {
+        clearInterval(timer);
+        if (typeof cb === 'function') {
+          cb();
+        }
+      }
+    }, options.delay);
+  }
+
+  init();
+};
